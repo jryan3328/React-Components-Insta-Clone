fc4521bb91c50ace112f2ab36e821c43a94e7963
Jonathan Ryan <jryan3328@gmail.com>
2021-02-16 17:43:26 -0500

    done

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -17,14 +17,14 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div className='like-section-wrapper' onClick={() => likePost()}>
           <FontAwesomeIcon icon={faHeart} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>{Posts.numberOfLikes}</p>
+      <p className='like-number'>{numberOfLikes}</p>
     </div>
   );
 };
